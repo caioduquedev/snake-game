afecbdd035b88e6b8ab6e2ad09922407a377817a
Caio Duque <caioduque.dev@gmail.com>
2020-06-01 11:08:35 -0300

    add final logic

--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
 using namespace std;
 
 bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+
 enum eDirection
 {
     STOP = 0,
     LEFT,
     RIGHT,
+    UP,
     DOWN
 };
+
 eDirection dir;
 
 void Setup()
@@ -40,7 +47,27 @@ void Draw()
         {
             if (j == 0)
                 cout << "#";
-            cout << " ";
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "F";
+            else
+            {
+                bool print = false;
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print)
+                {
+                    cout << " ";
+                }
+            }
+
             if (j == width - 1)
                 cout << "#";
         }
@@ -52,14 +79,117 @@ void Draw()
         cout << "#";
     }
     cout << endl;
+    cout << "SCORE: " << score << endl;
 }
 
 void Input()
 {
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            if (dir == RIGHT)
+            {
+                break;
+            }
+
+            dir = LEFT;
+            break;
+        case 'd':
+            if (dir == LEFT)
+            {
+                break;
+            }
+            dir = RIGHT;
+            break;
+        case 's':
+            if (dir == UP)
+            {
+                break;
+            }
+            dir = DOWN;
+            break;
+        case 'w':
+            if (dir == DOWN)
+            {
+                break;
+            }
+            dir = UP;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        }
+    }
 }
 
 void Logic()
 {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (dir)
+    {
+    case LEFT:
+        if (prevDir == RIGHT)
+        {
+            x++;
+            break;
+        }
+        x--;
+        break;
+    case RIGHT:
+        if (prevDir == LEFT)
+        {
+            x--;
+            break;
+        }
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
+
+    if (x > width || y > height || x < 0 || y < 0)
+    {
+        gameOver = true;
+    }
+
+    for (int i = 0; i < nTail; i++)
+    {
+        if (tailX[i] == x && tailY[i] == y)
+        {
+            gameOver = true;
+        }
+    }
+
+    if (x == fruitX && y == fruitY)
+    {
+        score += 1;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
 }
 int main()
 {
@@ -69,5 +199,6 @@ int main()
         Draw();
         Input();
         Logic();
+        Sleep(60);
     }
 }
